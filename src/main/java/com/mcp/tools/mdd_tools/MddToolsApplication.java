396f1df5aff14d2d8af86e03b238020de350ce1c
satvik <satvik140802@gmail.com>
2025-11-02 12:17:21 +0530

    initial commit

---
+++ src/main/java/com/mcp/tools/mdd_tools/MddToolsApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.tools.mdd_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MddToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MddToolsApplication.class, args);
+	}
+
+}
